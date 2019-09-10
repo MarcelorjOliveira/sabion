f202f546f541e9bfe08001ff0d1ccb6ef71c6c9b
Marcelo <marcelorj.oliveira@gmail.com>
2019-09-09 21:56:08 -0300

    Continuacao upload

--- tomcat/code/src/main/java/br/com/sabion/client/controller/UploadController.java
+++ tomcat/code/src/main/java/br/com/sabion/client/controller/UploadController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -37,6 +38,7 @@ public class UploadController {
     }
     
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    @ResponseBody
     public String singleFileUpload(@RequestParam("foto") MultipartFile file,
                                    RedirectAttributes redirectAttributes,
                                    HttpServletRequest request) {
@@ -44,10 +46,11 @@ public class UploadController {
     	System.out.println("Diretorio:");
 /*
 curl -F 'file=@/home/computador/Pictures/mastro.png' http://localhost/upload?token=4829448E-BAB4-4E59-B6A3-9C996B72DB71
-curl --cookie-jar newcookies.txt -F 'file=@/home/computador/Pictures/caso.png' http://localhost/upload
  */
     	String token = request.getParameter("token");
-
+    	
+    	String message = "";
+    	
 		Usuario usuario = userRepository.findByToken(token).get();
 
 		if(usuario != null) {
@@ -58,7 +61,8 @@ curl --cookie-jar newcookies.txt -F 'file=@/home/computador/Pictures/caso.png' h
 	    	
 	        if (file.isEmpty()) {
 	            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
-	            return "redirect:uploadStatus";
+	            message = "Please select a file to upload";
+	            //return "redirect:uploadStatus";
 	        }
 	
 	        try {
@@ -73,13 +77,15 @@ curl --cookie-jar newcookies.txt -F 'file=@/home/computador/Pictures/caso.png' h
 	
 	            redirectAttributes.addFlashAttribute("message", 
 	                        "You successfully uploaded '" + file.getOriginalFilename() + "'");
+	            
+	            message = "GetCwd : "+new java.io.File(".").getCanonicalPath()+" You successfully uploaded '"+diretorio + file.getOriginalFilename() + "'";
 	
 	        } catch (IOException e) {
 	            e.printStackTrace();
 	        }
 		}
 
-        return "redirect:/uploadStatus";
+        return message;
     }
 
     @RequestMapping("/uploadStatus")
