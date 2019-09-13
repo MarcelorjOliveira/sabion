ce6419b81395738273f0a386310698d75983d935
Marcelo <marcelorj.oliveira@gmail.com>
2019-09-13 13:54:21 -0300

    Aplicação como jar

--- tomcat/code/src/main/java/br/com/sabion/client/repository/BotRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.sabion.client.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.sabion.client.model.Bot;
-
-public interface BotRepository extends JpaRepository<Bot, Long> {
-	
-}
